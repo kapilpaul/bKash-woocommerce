249f0850a10c0b7ec0d9c0807957d4009ff01648
Kapil Paul <kapilpaul007@gmail.com>
2020-03-27 21:54:47 +0600

    fixed coding standards

--- includes/Base/BkashQuery.php
+++ includes/Base/BkashQuery.php
@@ -14,135 +14,141 @@ use Inc\WC_PGW_BKASH;
  * @package Inc\Base
  */
 class BkashQuery extends WC_PGW_BKASH {
-    private static $selfClassInstance;
+	private static $selfClassInstance;
 
-    /**
-     * @return BkashQuery
-     */
-    public static function getSelfClass() {
-        if ( ! self::$selfClassInstance ) {
-            return self::$selfClassInstance = ( new self );
-        }
-        return self::$selfClassInstance;
-    }
+	/**
+	 * @return BkashQuery
+	 */
+	public static function getSelfClass() {
+		if ( ! self::$selfClassInstance ) {
+			return self::$selfClassInstance = ( new self );
+		}
 
-    /**
-     * Grant Token Url
-     *
-     * @return string
-     */
-    public static function grantTokenUrl() {
-        $selfClass = self::getSelfClass();
-        if ( $selfClass->get_option( 'test_mode' ) == 'on' ) {
-            return 'https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/token/grant';
-        }
-        return 'https://checkout.pay.bka.sh/v1.2.0-beta/checkout/token/grant';
-    }
+		return self::$selfClassInstance;
+	}
 
-    /**
-     * Payment Query Url
-     *
-     * @return string
-     */
-    public static function paymentQueryUrl() {
-        $selfClass = self::getSelfClass();
-        if ( $selfClass->get_option( 'test_mode' ) == 'on' ) {
-            return 'https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/payment/query/';
-        }
-        return 'https://checkout.pay.bka.sh/v1.2.0-beta/checkout/payment/query/';
-    }
+	/**
+	 * Grant Token Url
+	 *
+	 * @return string
+	 */
+	public static function grantTokenUrl() {
+		$selfClass = self::getSelfClass();
+		if ( $selfClass->get_option( 'test_mode' ) == 'on' ) {
+			return 'https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/token/grant';
+		}
 
-    /**
-     * Get Token
-     *
-     * @return bool|mixed
-     */
-    public static function getToken() {
-        if ( $token = get_transient( 'bkash_token' ) ) {
-            return $token;
-        }
+		return 'https://checkout.pay.bka.sh/v1.2.0-beta/checkout/token/grant';
+	}
 
-        $selfClass = self::getSelfClass();
-        $userName  = $selfClass->get_option( 'username' );
-        $password  = $selfClass->get_option( 'password' );
+	/**
+	 * Payment Query Url
+	 *
+	 * @return string
+	 */
+	public static function paymentQueryUrl() {
+		$selfClass = self::getSelfClass();
+		if ( $selfClass->get_option( 'test_mode' ) == 'on' ) {
+			return 'https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/payment/query/';
+		}
 
-        $data = [
-            "app_key"    => $selfClass->get_option( 'app_key' ),
-            "app_secret" => $selfClass->get_option( 'app_secret' ),
-        ];
+		return 'https://checkout.pay.bka.sh/v1.2.0-beta/checkout/payment/query/';
+	}
 
-        $headers = [
-            "username"     => $userName,
-            "password"     => $password,
-            "Content-Type" => "application/json",
-        ];
+	/**
+	 * Get Token
+	 *
+	 * @return bool|mixed
+	 */
+	public static function getToken() {
+		if ( $token = get_transient( 'bkash_token' ) ) {
+			return $token;
+		}
 
-        $result = self::makeRequest( self::grantTokenUrl(), $data, $headers );
+		$selfClass = self::getSelfClass();
+		$userName  = $selfClass->get_option( 'username' );
+		$password  = $selfClass->get_option( 'password' );
 
-        if ( isset( $result['id_token'] ) && isset( $result['token_type'] ) ) {
-            $token = $result['id_token'];
-            set_transient( 'bkash_token', $token, $result['expires_in'] );
-            return $result['id_token'];
-        }
+		$data = [
+			"app_key"    => $selfClass->get_option( 'app_key' ),
+			"app_secret" => $selfClass->get_option( 'app_secret' ),
+		];
 
-        return false;
-    }
+		$headers = [
+			"username"     => $userName,
+			"password"     => $password,
+			"Content-Type" => "application/json",
+		];
 
-    /**
-     * sending curl request
-     *
-     * @param $url
-     * @param $data
-     * @param array $headers
-     *
-     * @return mixed|string
-     */
-    public static function makeRequest( $url, $data, $headers = [] ) {
-        $args = array(
-            'body'        => json_encode( $data ),
-            'timeout'     => '15',
-            'redirection' => '15',
-            'httpversion' => '1.0',
-            'blocking'    => true,
-            'headers'     => $headers,
-            'cookies'     => [],
-        );
+		$result = self::makeRequest( self::grantTokenUrl(), $data, $headers );
 
-        $response = wp_remote_retrieve_body( wp_remote_post( $url, $args ) );
-        return json_decode( $response, true );
-    }
+		if ( isset( $result['id_token'] ) && isset( $result['token_type'] ) ) {
+			$token = $result['id_token'];
+			set_transient( 'bkash_token', $token, $result['expires_in'] );
 
-    /**
-     * verify payment on bKash end
-     *
-     * @param $paymentID
-     * @param $orderTotal
-     *
-     * @return bool|mixed|string
-     */
-    public static function verifyPayment( $paymentID, $orderTotal ) {
-        $selfClass = self::getSelfClass();
-        if ( $selfClass->get_option( 'test_mode' ) == 'on' ) {
-            return [ 'amount' => $orderTotal ];
-        }
+			return $result['id_token'];
+		}
 
-        if ( $token = self::getToken() ) {
-            $headers = [
-                "Authorization" => "Bearer {$token}",
-                "X-App-Key"     => $selfClass->get_option( 'app_key' ),
-            ];
+		return false;
+	}
 
-            $args = array(
-                'headers' => $headers,
-            );
+	/**
+	 * sending curl request
+	 *
+	 * @param $url
+	 * @param $data
+	 * @param array $headers
+	 *
+	 * @return mixed|string
+	 */
+	public static function makeRequest( $url, $data, $headers = [] ) {
+		$args = array(
+			'body'        => json_encode( $data ),
+			'timeout'     => '15',
+			'redirection' => '15',
+			'httpversion' => '1.0',
+			'blocking'    => true,
+			'headers'     => $headers,
+			'cookies'     => [],
+		);
 
-            $response = wp_remote_get( self::paymentQueryUrl() . $paymentID, $args );
-            $result   = json_decode( wp_remote_retrieve_body( $response ), true );
+		$response = wp_remote_retrieve_body( wp_remote_post( $url, $args ) );
 
-            if ( ! isset( $result['errorCode'] ) && ! isset( $result['errorMessage'] ) ) {
-                return $result;
-            }
-        }
-        return false;
-    }
+		return json_decode( $response, true );
+	}
+
+	/**
+	 * verify payment on bKash end
+	 *
+	 * @param $paymentID
+	 * @param $orderTotal
+	 *
+	 * @return bool|mixed|string
+	 */
+	public static function verifyPayment( $paymentID, $orderTotal ) {
+		$selfClass = self::getSelfClass();
+		if ( $selfClass->get_option( 'test_mode' ) == 'on' ) {
+			return [ 'amount' => $orderTotal ];
+		}
+
+		if ( $token = self::getToken() ) {
+			$headers = [
+				"Authorization" => "Bearer {$token}",
+				"X-App-Key"     => $selfClass->get_option( 'app_key' ),
+			];
+
+			$args = array(
+				'headers' => $headers,
+			);
+
+			$response = wp_remote_get( self::paymentQueryUrl() . $paymentID, $args );
+			$result   = json_decode( wp_remote_retrieve_body( $response ), true );
+
+			if ( ! isset( $result['errorCode'] ) && ! isset( $result['errorMessage'] ) ) {
+				return $result;
+			}
+		}
+
+		return false;
+	}
 }
